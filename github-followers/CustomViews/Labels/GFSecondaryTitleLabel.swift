1844ace17f79a325ce382e7fbef5fd7b80984b8e
Mark Battistella <markbattistella@users.noreply.github.com>
2021-10-09 18:13:20 +1100

    finished #33

---
+++ github-followers/CustomViews/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,41 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  github-followers
+//
+//  Created by Mark Battistella on 9/10/21.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+	override init(frame: CGRect) {
+		
+		// call the super class
+		// -- all the defaults
+		super.init(frame: frame)
+		
+		// set our defaults
+		configure()
+	}
+	
+	// for storyboard
+	required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+	
+	// allow customisation
+	init(fontSize: CGFloat) {
+		super.init(frame: .zero)
+		font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+		configure()
+	}
+	
+	// func: default customisation
+	private func configure() {
+		textColor = .secondaryLabel
+		adjustsFontSizeToFitWidth = true
+		minimumScaleFactor = 0.9
+		lineBreakMode = .byTruncatingTail
+		translatesAutoresizingMaskIntoConstraints = false
+	}
+
+}
---
+++ github-followers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  github-followers
+//
+//  Created by Mark Battistella on 9/10/21.
+//
+
+import Foundation
+
+enum SFSymbols {
+	static let location = "pin.fill"
+}
